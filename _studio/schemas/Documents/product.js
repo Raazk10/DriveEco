4d5ed598b8d45e5b4f0b7fbf20f3b753ae7f64e6
Raazk10 <rakioraag10@gmail.com>
2023-03-28 11:19:47 +0200

    added  product

--- _studio/schemas/Documents/product.js
+++ _studio/schemas/Documents/product.js
@@ -18,6 +18,12 @@ export default {
       },
     },
     {
+      title: "SubTitle",
+      name: "subtitle",
+      type: "string",
+    },
+
+    {
       title: "Image",
       name: "image",
       type: "image",
@@ -26,8 +32,19 @@ export default {
       },
     },
     {
+      title: "Images",
+      name: "images",
+      type: "array",
+      of: [{ type: "image", options: { hotspot: true } }],
+    },
+    {
       title: "Description",
       name: "description",
+      type: "text",
+    },
+    {
+      title: "Warrenty",
+      name: "warrenty",
       type: "string",
     },
 
@@ -37,14 +54,76 @@ export default {
       type: "number",
     },
     {
-      title: "CountInStock",
-      name: "countInStock",
-      type: "number",
-    },
-    {
       title: "Brand",
       name: "brand",
       type: "string",
     },
+    {
+      title: "Model",
+      name: "model",
+      type: "string",
+    },
+    {
+      title: "Fuel",
+      name: "fuel",
+      type: "string",
+    },
+    {
+      title: "Km Stand",
+      name: "kmstand",
+      type: "string",
+    },
+    {
+      title: "Power",
+      name: "power",
+      type: "string",
+    },
+    {
+      title: "Wheel drive",
+      name: "wheeldrive",
+      type: "string",
+    },
+    {
+      title: "Gearbox",
+      name: "gearbox",
+      type: "string",
+    },
+    {
+      title: "Body Condition",
+      name: "bodyCondition",
+      type: "string",
+    },
+    {
+      title: "Mechanical Condition",
+      name: "mechanicalCondition",
+      type: "string",
+    },
+    {
+      title: "Category",
+      name: "category",
+      type: "reference",
+      to: [{ type: "category" }],
+    },
+    {
+      title: "Equipment",
+      name: "equipment",
+      type: "array",
+      of: [{ type: "reference", to: [{ type: "equipment" }] }],
+    },
   ],
+  preview: {
+    select: {
+      name: "name",
+      image: "image",
+      price: "price",
+    },
+    prepare(selection) {
+      const { name, image, price } = selection;
+      return {
+        title: name,
+        subtitle: `${price.toFixed(2)} kr`,
+        media: image,
+      };
+    },
+  },
 };
--- _studio/schemas/schemas.js
+++ _studio/schemas/schemas.js
@@ -1,2 +1,4 @@
+import category from "./Documents/category.js";
+import equipment from "./Documents/equipment.js";
 import product from "./Documents/product.js";
-export default [product];
+export default [product, category, equipment];
