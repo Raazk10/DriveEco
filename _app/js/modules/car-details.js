cf9ebbe1ffa2efbd428757a140d52e0487303648
Darin Hajou <it_related@hotmail.com>
2023-04-11 21:51:51 +0200

    Update elements on details page and add BEM

--- _app/js/modules/car-details.js
+++ _app/js/modules/car-details.js
@@ -3,6 +3,17 @@ import { sanity } from "../sanity.js";
 const searchParams = new URLSearchParams(window.location.search);
 const id = searchParams.get("id");
 
+function displayCarDetail(carDetail) {
+  const descriptionContainer = document.createElement("div");
+
+  const descriptionParagraph = document.createElement("p");
+  descriptionParagraph.textContent = carDetail.description;
+
+  descriptionContainer.appendChild(descriptionParagraph);
+  
+  return descriptionContainer;
+}
+
 async function fetchCarDetails() {
   const query = `*[ _type == "product" && _id == "${id}" ]{
     _id,
@@ -15,22 +26,35 @@ async function fetchCarDetails() {
     enginePower,
     wheeldrive,
     price,
-    "image": image[0].asset->url,
+    description,
+    "images": image[].asset->url,
   }`;
   const carDetails = await sanity.fetch(query);
 
-  const carImage = document.querySelector(".car-image");
-  const carTitle = document.querySelector(".car-title");
-  const carModelYear = document.querySelector(".car-model-year");
-  const carBrand = document.querySelector(".car-brand");
-  const carDriven = document.querySelector(".car-driven");
-  const carGearbox = document.querySelector(".car-gearbox");
-  const carFuel = document.querySelector(".car-fuel");
-  const carEnginePower = document.querySelector(".car-engine-power");
-  const carWheelDrive = document.querySelector(".car-wheel-drive");
-  const carPrice = document.querySelector(".car-price");
+  const carImages = document.querySelector(".car__images");
 
-  carImage.src = carDetails[0].image;
+  carDetails[0].images.forEach((imageUrl) => {
+    const img = document.createElement("img");
+    img.src = imageUrl;
+    img.alt = `${carDetails[0].name} image`;
+    carImages.appendChild(img);
+  });
+
+  const carTitle = document.querySelector(".car__title");
+  const carModelYear = document.querySelector(".car__model-year");
+  const carBrand = document.querySelector(".car__brand");
+  const carDriven = document.querySelector(".car__driven");
+  const carGearbox = document.querySelector(".car__gearbox");
+  const carFuel = document.querySelector(".car__fuel");
+  const carEnginePower = document.querySelector(".car__engine-power");
+  const carWheelDrive = document.querySelector(".car__wheel-drive");
+  const carPrice = document.querySelector(".car__price");
+
+  const carDescriptionContainer = displayCarDetail(carDetails[0]);
+
+  const carDescriptionWrapper = document.querySelector(".car__description--container");
+  carDescriptionWrapper.appendChild(carDescriptionContainer);
+
   carTitle.textContent = carDetails[0].name;
   carModelYear.textContent = carDetails[0].modelYear;
   carBrand.textContent = carDetails[0].brand;
